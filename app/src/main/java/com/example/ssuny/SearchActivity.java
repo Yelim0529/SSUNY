839ec2c3fa2d47a79a07bed925100021b1967764
nahyun0423 <skgus0423@naver.com>
2023-06-02 19:18:52 +0900

    test

--- app/src/main/java/com/example/ssuny/SearchActivity.java
+++ app/src/main/java/com/example/ssuny/SearchActivity.java
@@ -11,10 +11,18 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
 public class SearchActivity extends AppCompatActivity {
 
     private EditText editTextSearch;
-
+    private List<String> items = Arrays.asList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,5 +62,99 @@ public class SearchActivity extends AppCompatActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //------------
+    StringBuilder output = new StringBuilder(); //새로 추가
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SearchView searchView = findViewById(R.id.search_view);
+        TextView resultTextView = findViewById(R.id.textView);
+        resultTextView.setText(getResult());
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) { //검색버튼 누르면 검색한 단어 나옴
+                System.out.println(query);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        request(query); //쿼리로 변경
+                    }
+                }).start();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) { //실시간으로 타자치는거 뉴텍스트로 들어옴.
+                resultTextView.setText(search(newText));
+                return true;
+            }
+        });
+    }
+
+    private String search(String query) { //포함된 단어만 보이게 필터링
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < items.size(); i++) {
+            String item = items.get(i);
+            if (item.toLowerCase().contains(query.toLowerCase())) {
+                sb.append(item);
+                if (i != items.size() - 1) {
+                    sb.append("\n");
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    private String getResult() { // textview 보이기
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < items.size(); i++) {
+            String item = items.get(i);
+            sb.append(item);
+            if (i != items.size() - 1) {
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    public void request(String query) {
+        try {
+            URL url = new URL("http://52.78.100.143:8080/api/searchDrug/" + query);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            if (connection != null) {
+                connection.setConnectTimeout(10000);
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Accept", "application/json");
+                connection.setDoInput(true);
+
+                int resCode = connection.getResponseCode();
+                if (resCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String line = null;
+                    while (true) {
+                        line = reader.readLine();
+                        if (line == null) {
+                            break;
+                        }
+
+                        output.append(line);
+                    }
+                    reader.close();
+                    connection.disconnect();
+                } else {
+                    System.out.println(resCode); //현재 요청 상태
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.toString()); //예외 발생
+        }
+
+        System.out.println(output.toString()); //응답
     }
 }
